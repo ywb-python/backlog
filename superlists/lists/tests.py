7eb63c6dc98d504d2873f8470f4a1d0093a5352c
unknown <18721706546@163.com>
2021-02-07 21:23:43 +0800

    使用模板重写视图

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,21 +2,22 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found, home_page)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
-        self.assertEqual('<title>To-Do lists</title>', html)
+        self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
 
 
 # Create your tests here.
