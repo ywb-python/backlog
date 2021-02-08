673e40dfe32745d89dfb5673c5515407630ea89c
unknown <18721706546@163.com>
2021-02-08 22:02:10 +0800

    将变量传入模板进行渲染

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -30,11 +30,19 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make fly')
+        input_box.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows),
-                        'New to-do item did not appear in table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])
+
+
         self.fail('Finish the test!')
 
 
