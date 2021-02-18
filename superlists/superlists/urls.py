5ce104bcdf55c4b7c9b6532c4c66daac77ea1a25
unknown <18721706546@163.com>
2021-02-18 21:38:17 +0800

    新的用于展示所有带班事项的模板、视图和url

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -21,5 +21,6 @@ from lists import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    url(r'^$', views.home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
 ]
