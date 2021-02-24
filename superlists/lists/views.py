21e63db03a06830136c0b254e256986d68dbb8da
unknown <18721706546@163.com>
2021-02-24 22:23:16 +0800

    修改之前先保存

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -13,5 +13,10 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
 
 # Create your views here.
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -22,5 +22,6 @@ from lists import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^lists/new$', views.new_list, name='new_list')
 ]
