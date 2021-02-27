ba5dd8d6581668d9f11bf18e7075621377655196
unknown <18721706546@163.com>
2021-02-27 22:11:29 +0800

    新增一个测试页面布局的功能测试

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -81,3 +81,16 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('testing')
+        input_box.send_keys(Keys.ENTER)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
