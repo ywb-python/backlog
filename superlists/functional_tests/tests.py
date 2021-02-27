a154e31468b014ac28a544c69f4d635889814ec9
unknown <18721706546@163.com>
2021-02-27 11:58:00 +0800

    修改第6章的问题

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(LiveServerTestCase):
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
-            time.sleep(0.5)
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -44,14 +44,17 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('Use peacock feathers to make fly')
+        input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make fly')
         self.fail('Finish the test!')
 
