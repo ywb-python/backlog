82c1a85c3e39bbbc51754e058a2ef3a35b90d4d1
unknown <18721706546@163.com>
2021-02-28 11:20:11 +0800

    使用bootstrap调整布局

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -17,7 +17,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
