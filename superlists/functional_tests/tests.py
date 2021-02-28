e899a364e9dc4980595d5285ffda864ff620da19
unknown <18721706546@163.com>
2021-02-28 14:47:51 +0800

    指定了临时服务器的地址

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -12,6 +12,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 
 MAX_WAIT = 10
@@ -21,6 +22,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
+        staging_server = os.environ.get('STAGING')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
