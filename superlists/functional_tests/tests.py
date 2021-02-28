d30ad737dde3600c35f403aa8589004a20a69593
unknown <18721706546@163.com>
2021-02-28 11:35:31 +0800

    修改不合理的地方

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -86,6 +86,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
         input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
         input_box.send_keys('testing')
         input_box.send_keys(Keys.ENTER)
         input_box = self.browser.find_element_by_id('id_new_item')
