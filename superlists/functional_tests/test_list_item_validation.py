0384ce4a71d7f5625eff3d09b58f656b5d32fb48
unknown <18721706546@163.com>
2021-03-03 23:07:24 +0800

    第12章进行修改

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -11,24 +11,45 @@ from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 
 
-MAX_WAIT = 10
-
-
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
-        self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
-                         "You can't have empty list item"))
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # The browser intercepts the request, and does not load the
+        # list page
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
+
+        # She starts typing some text for the new item and the error disappears
+        self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        # And she can submit it successfully
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
-                         "You can't have empty list item"))
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # Perversely, she now decides to submit a second blank list item
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # Again, the browser will not comply
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
+
+        # And she can correct it by filling some text in
+        self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
-        self.fail('finsh this test!')
+
---
+++ superlists/lists/forms.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/3 21:07
+# @Author  : ywb
+# @Site    : 
+# @File    : forms.py
+# @Software: PyCharm
+
+
+from django import forms
+from lists.models import Item
+
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
