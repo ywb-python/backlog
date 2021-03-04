4aaaedcb661bd53eef68fdb8144e79edc2d9fe4b
unknown <18721706546@163.com>
2021-03-04 23:39:51 +0800

    表单修改

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -16,7 +16,6 @@ class ItemValidationTest(FunctionalTest):
     def get_error_element(self):
         return self.browser.find_element_by_css_selector('.has-error')
 
-
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
@@ -54,24 +53,17 @@ class ItemValidationTest(FunctionalTest):
             "You've already got this in your list"
         ))
 
-
     def test_error_messages_are_cleared_on_input(self):
-        # Edith starts a list and causes a validation error:
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Banter too thick')
         self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
-
         self.wait_for(lambda: self.assertTrue(
             self.get_error_element().is_displayed()
         ))
-
-        # She starts typing in the input box to clear the error
         self.get_item_input_box().send_keys('a')
-
-        # She is pleased to see that the error message disappears
         self.wait_for(lambda: self.assertFalse(
             self.get_error_element().is_displayed()
         ))
--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -9,7 +9,7 @@ class List(models.Model):
 
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default='', unique=True)
     list = models.ForeignKey(List, default=None,on_delete=models.CASCADE)
 
     class Meta:
