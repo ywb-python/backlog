8603c4dc4e3ae1066e4d977f8b255b20a1871e60
18721706546@163.com <ywb1718951687>
2021-03-16 22:41:12 +0800

    为单元测试加注释

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,10 +7,17 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page(self):
+        """
+        测试网站根路径("/")能否被正确解析，映射到对应的视图函数上
+        """
+        # resolve():解析url，并将其映射到对应的视图函数上
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        """
+        测试视图函数home_page()返回内容的正确性
+        """
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
