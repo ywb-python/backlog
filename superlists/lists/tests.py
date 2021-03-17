29f05e6217849d184a393838dee32f0eff1e8917
18721706546@163.com <ywb1718951687>
2021-03-17 22:55:13 +0800

    更新单元测试的注释

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,10 +5,10 @@ class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         """
-        测试网站根路径("/")能否被正确解析，映射到对应的视图函数上
+        测试网站根路径("/")能否被正确解析，映射到对应的视图函数上，并测试所渲染模板的正确性
         """
-        # assertTemplateUsed():检查响应是使用哪个模板进行渲染的
         response = self.client.get('/')
+        # assertTemplateUsed():检查响应是使用哪个模板进行渲染的
         self.assertTemplateUsed(response, 'home.html')
 
 # Create your tests here.
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def home_page(request):
+    """
+    对应'/'的视图函数,渲染首页
+    """
     return render(request, 'home.html')
 
 
