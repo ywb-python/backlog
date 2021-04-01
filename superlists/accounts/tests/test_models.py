d5f7484f928f552db6f1376f8732be0ac9a7f569
18721706546@163.com <ywb1718951687>
2021-04-01 23:17:45 +0800

    用户验证的更新

---
+++ superlists/accounts/tests/test_models.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/1 23:10
+# @Author  : ywb
+# @Site    : 模型测试
+# @File    : test_models.py
+# @Software: PyCharm
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from accounts.models import Token
+
+User = get_user_model()
+
+
+class UserModelTest(TestCase):
+    """
+    模型User的测试
+    """
+
+    def test_user_is_valid_with_email_only(self):
+        """
+        测试只有一个邮箱信息的用户是否合法
+        """
+
+        user = User(email='18721706546@163.com')
+        user.full_clean()
+
+    def test_email_is_primary_key(self):
+        """
+        测试设置email字段为主键时的情形
+        """
+        user = User(email='18721706546@163.com')
+        self.assertEqual(user.pk, '18721706546@163.com')
+
+
+class TokenModelTest(TestCase):
+    """
+    模型Token的测试
+    """
+
+    def test_links_user_with_auto_generated_uid(self):
+        """
+        测试邮件地址每次是否可以关联到唯一的uid,
+        """
+        token1 = Token.objects.create(email='18721706546@163.com')
+        token2 = Token.objects.create(email='18721706546@163.com')
+        self.assertNotEqual(token1.uid, token2.uid)
---
+++ superlists/functional_tests/test_login.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/1 23:06
+# @Author  : ywb
+# @Site    : 登录相关的测试
+# @File    : test_login.py
+# @Software: PyCharm
+
+
+from django.core import mail
+from selenium.webdriver.common.keys import Keys
+import re
+from .base import FunctionalTest
+
+
+TEST_EMAIL = '18721706546@163.com'
+SUBJECT = 'Your login link for Superlists'
+
+
+class LoginTest(FunctionalTest):
+    """
+    登录测试
+    """
+
+    def test_can_get_email_link_to_login_in(self):
+        """
+        测试点击邮件链接进行登录
+        """
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertIn(
+            "Check your email", self.browser.find_element_by_tag_name('body').text
+        ))
+        email = mail.outbox[0]
+        self.assertIn(TEST_EMAIL, email.to)
+        self.assertEqual(email.subject, SUBJECT)
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail(f'Could not find url in email body:\n{email.body}')
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)
+        self.browser.get(url)
+        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(TEST_EMAIL, navbar.text)
