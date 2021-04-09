6af3836667ce71fe0c7c81a5da13f07ecc14fc94
18721706546@163.com <ywb1718951687>
2021-04-09 23:44:58 +0800

    1.增加了requirements.txt

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/2/6 20:41
 # @Author  : ywb
-# @Site    : 
+# @Site    : 启动Django开发服务器并在浏览器中查看
 # @File    : functional_tests.py
 # @Software: PyCharm
 
 
 from selenium import webdriver
+
 
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
