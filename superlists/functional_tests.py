3476becee0def53ba82d2a5a76e8a72a3ae902bd
18721706546@163.com <ywb1718951687>
2021-04-10 09:57:31 +0800

    恢复原始状态

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -6,27 +6,3 @@
 # @File    : functional_tests.py
 # @Software: PyCharm
 
-
-from selenium import webdriver
-import unittest
-
-
-class NewVisitorTest(unittest.TestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """
-        测试首页标题的正确性
-        """
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -13,10 +13,4 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
 
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
